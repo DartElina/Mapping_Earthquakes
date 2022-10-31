5881aef8e4ac13002cd0e47d6486d36c78c804c1
elina dart <dartelina4@gmail.com>
2022-10-31 14:16:45 -0400

    blue dashed line for travel SFO to JFK 2 stops on the way

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,29 +1,43 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
-// add map to page
-var map = L.map('map').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('map').setView([37.6213, -122.3790], 5);
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6413, -73.7781]
+  ];
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "blue",
+    dashArray: [6,6],
+    weight: 4,
+    opacity: .5
+}).addTo(map);
 
 // Get data from cities.js
-let cityData = cities;
+//let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        color: 'orange',
-        fillColor: 'orange',
-        fillOpacity: .2,
-        radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+// cityData.forEach(function (city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         color: 'orange',
+//         fillColor: 'orange',
+//         fillOpacity: .2,
+//         radius: city.population / 100000
+//     })
+//         .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//         .addTo(map);
+// });
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/satellite-streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
